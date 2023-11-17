7bf3f510200d88e25b36a698e18b51669a7123cb
githubtestdg <arslan.dgn56@gmail.com>
2023-11-18 01:25:46 +0300

    Added checkbox

--- src/App.js
+++ src/App.js
@@ -15,13 +15,32 @@ export default function App() {
     {key : 4, value: 'HTML'}
   ]
 
+
   
   const [name, setName] = useState('Dgn')
   const [description, setDescription] = useState('')
   const [gender, setGender] = useState('')
   const [category, setCategory] = useState([2, 4])
+  const [rule, setRule] = useState(false)
+  const [rules, setRules] = useState([
+    {key : 1 , value: '1. kuralı kabul ediyorum', checked: false},
+    {key : 2 , value: '2. kuralı kabul ediyorum', checked: false},
+    {key : 3 , value: '3. kuralı kabul ediyorum', checked: true},
+  ])
   
+  const selectedCategory = category && categoryList.filter(cat => category.includes(cat.key))
   const selectGender = genders.find(g => g.key === gender)
+
+  const checkRule = (key,checked) => {
+    setRules(rules => rules.map(rule => {
+      if(key === rule.key){
+        rule.checked = checked
+      }
+      return rule
+    }))
+  }
+
+  const enabled = rules.every(rule => rule.checked)
 
 
   return (
@@ -33,7 +52,7 @@ export default function App() {
       <input type="text" value={name} onChange={e => setName(e.target.value)} />
       <br />
       <hr />
-      <textarea value={description}></textarea>
+      <textarea defaultValue={description}></textarea>
       <br />
       <select value={gender} onChange={e => setGender(e.target.value)}>
         <option value="">Seçiniz</option>
@@ -43,15 +62,35 @@ export default function App() {
       </select>
         <hr />
       <br />
-      <select value={category} multiple={true} onChange={e => setCategory([...e.target.selectedOptions].map(option => option.value))}>
+      <button onClick={() => setCategory([2,3])}>Kategöri seç</button>
+      
+      <select value={category} multiple={true} onChange={e => setCategory([...e.target.selectedOptions].map(option => +option.value))}>
         {categoryList.map(category => (
           <option value={category.key} key={category.key}> {category.value}</option>
         ))}
       </select>
 
       <pre>
-        {JSON.stringify(category, null , 2)}
+        {JSON.stringify(selectedCategory , null , 2)}
       </pre>
+      <br />
+      <hr />
+      <label>
+          <input type="checkbox"  checked={rule} onChange={e  => setRule(e.target.checked)} />
+          Kuralları kabul ediyorum
+      </label>
+      <br />
+      <button disabled={!rule}>Devam Et</button>
+      <br />
+      <hr />
+      {rules.map(rule => (
+      <label key={rule.key}>
+        <input type="checkbox"  checked={rule.checked} onChange={e => checkRule(rule.key, e.target.checked)}/>
+          {rule.value}
+        </label>
+      ))}
+      <br />
+      <button disabled={!enabled}>Devam Et</button>
     </>
   ); 
 }
